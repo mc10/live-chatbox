6fc97cd7c1e68ff54f833b4ebbd0eff2eca48caf
Kevin Ji <kevin.ji@outlook.com>
2014-10-04 02:05:24 -0700

    Fix Firebase integration

--- js/main.js
+++ js/main.js
@@ -17,5 +17,12 @@ require(["angularfire"], function() {
 	app.controler("ChatCtrl", function($scope, $firebase) {
 		var ref = new Firebase("https://blistering-heat-665.firebaseio.com/data");
 		var sync = $firebase(ref);
+
+		// Sync messages as an array
+		$scope.messages = sync.$asArray();
+
+		$scope.sendMessage = function(message) {
+			$scope.messages.$add({ message: message });
+		};
 	});
 });
